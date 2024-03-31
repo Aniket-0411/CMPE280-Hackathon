43aef8588b00314c95addf56223e6c5c17f78291
evolentDelight <evolentDelight@outlook.com>
2024-03-30 18:05:27 -0700

    remove irrelevant code

--- Ben Temp/src/script.js
+++ Ben Temp/src/script.js
@@ -1,27 +1,6 @@
-//When ChatGPT AI's response is received, load message onto front-end
-function AI_Response(response) {
-  const response_element = document.getElementById("response");
-
-  response_element.textContent = response;
-}
-
-//Extract Prompt from front-end
-function extractPrompt() {
-  const prompt = document.getElementById("prompt").value;
-
-  if (prompt) {
-    return prompt;
-  }
-
-  alert("Please Enter Prompt");
-  return null;
-}
-
 //AJAX to ChatGPT
 //Call webserver to process Prompt
-async function inquirePrompt() {
-  const prompt = extractPrompt();
-
+async function inquirePrompt(prompt) {
   if (prompt) {
     //REST call
     await fetch("https://chatgpt-chat-us-tax-law.onrender.com/", {
@@ -39,23 +18,3 @@ async function inquirePrompt() {
       });
   }
 }
-
-//Cancel Prompt by erasing prompt
-function cancelPrompt() {
-  document.getElementById("prompt").value = "";
-}
-
-//Event Listeners
-//Cancel - Remove prompt
-document.getElementById("cancelButton").addEventListener("click", cancelPrompt);
-
-//Inquire/Submit button
-document
-  .getElementById("submitButton")
-  .addEventListener("click", inquirePrompt);
-document.getElementById("prompt").addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    e.preventDefault(); //Disables newline entered once "Enter" key is keyed
-    inquirePrompt();
-  }
-});
