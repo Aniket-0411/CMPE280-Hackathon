0322cc1ad2878eaff49422bfdee88638d6d8961a
Aniket Chopade <ani.chopade57@gmail.com>
2024-03-30 18:24:44 -0700

    Update Index.HTML wil Peizu's CSS

--- Aniket Temp/Hackathon code/index.js
+++ Aniket Temp/Hackathon code/index.js
@@ -1,26 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Event listener for GDP link
+    document.getElementById('gdpLink').addEventListener('click', function(event) {
+        event.preventDefault();
+        document.getElementById('Frame').data = 'GDP.html';
+    });
 
-// Event listener for GDP link
-document.getElementById('gdpLink').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.getElementById('Frame').src = 'GDP.html';
-});
+    // Event listener for macroeconomic menu item
+    document.querySelectorAll('.expandable').forEach(function(item) {
+        item.addEventListener('click', function() {
+            this.classList.toggle('active'); // Toggle the active class
+            const submenu = this.nextElementSibling;
+            submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block'; // Toggle display of submenu
+        });
+    });
 
-// Event listener for macroeconomic menu item
-document.querySelector('.sidebar ul li.expandable1').addEventListener('click', function() {
-    this.classList.toggle('active'); // Toggle the active class
-    const submenu = this.querySelector('.submenu1');
-    submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block'; // Toggle display of submenu
-});
+    // Event listener for FDI Inflows link
+    document.getElementById('fdiInflowsLink').addEventListener('click', function(event) {
+        event.preventDefault();
+        document.getElementById('Frame').data = 'FDI_Inflows.html';
+        // Add functionality to plot FDI Inflows graph
+    });
 
-// Event listener for FDI Inflows link
-document.getElementById('fdiInflowsLink').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.getElementById('Frame').src = 'FDI_Inflows.html';
-});
+    // Event listener for FDI Outflows link
+    document.getElementById('fdiOutflowsLink').addEventListener('click', function(event) {
+        event.preventDefault();
+        document.getElementById('Frame').data = 'FDI_Outflows.html';
+        // Add functionality to plot FDI Outflows graph
+    });
 
-// Event listener for FDI Outflows link
-document.getElementById('fdiOutflowsLink').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.getElementById('Frame').src = 'FDI_Outflows.html';
-    // Add functionality to plot FDI Outflows graph
+
 });
