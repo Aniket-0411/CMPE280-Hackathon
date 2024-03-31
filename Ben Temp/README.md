9fb6869a017a398dafb01f7970bfb5157d2ab09f
evolentDelight <evolentDelight@outlook.com>
2024-03-30 17:54:14 -0700

    initial commit

---
+++ Ben Temp/README.md
@@ -0,0 +1,5 @@
+# Webserver + API server
+
+To spin up the server:<br>
+npm install<br>
+node app.js
---
+++ Ben Temp/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(express.static(__dirname + "/src"));
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/src/index.html"));
+
+app.post("/", async (req, res) => {
+  //Process the prompt
+  let prompt = req.body.prompt;
+
+  let response = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system", //Prompt Engineering
+        content: "{Prompt Engineering}",
+      },
+      {
+        role: "user",
+        content: `${prompt}`,
+      },
+    ],
+  };
+
+  await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.CHATGPT_API_KEY}`,
+    },
+    body: JSON.stringify(response),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      //Handle Response
+      const data = {
+        response: json.choices[0].message.content,
+      };
+      res.send({ data });
+    });
+});
+
+app.listen(port);
